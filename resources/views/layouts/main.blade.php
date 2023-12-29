38f21d98cb8f74ded0997fcbf9b96f477e4e57fe
DimaKravackij <kravatsky.d.wezom@gmail.com>
2023-12-29 16:23:19 +0200

    fix nav-bar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,18 +35,31 @@
     <div class="hero_area">
         <header class="header_section long_section px-0">
             <nav class="navbar navbar-expand-lg custom_nav-container ">
-                <a class="navbar-brand" href="{{ route('home') }}">
-                    <span>{{ config('app.name') }}</span>
-                </a>
+                @php($notHomePage = request()->route()->getName() !== 'home')
+                @if($notHomePage)
+                    <a class="navbar-brand" href="{{ request()->route()->getName() !== 'home' ? route('home') : '#' }}">
+                        <span>{{ config('app.name') }}</span>
+                    </a>
+                @else
+                    <span class="navbar-brand">
+                        <span>{{ config('app.name') }}</span>
+                    </span>
+                @endif
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class=""> </span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
                         <ul class="navbar-nav  ">
-                            <li class="nav-item ">
-                                <a class="nav-link" href="{{ route('home') }}">Головна<span class="sr-only">(current)</span></a>
-                            </li>
+                            @if($notHomePage)
+                                <li class="nav-item ">
+                                    <a class="nav-link" href="{{ route('home') }}">Головна<span class="sr-only">(current)</span></a>
+                                </li>
+                            @else
+                                <li class="nav-item ">
+                                    <span class="nav-link">Головна<span class="sr-only">(current)</span></span>
+                                </li>
+                            @endif
                             <li class="nav-item active">
                                 <a class="nav-link" href="{{ route('about') }}">Про нас</a>
                             </li>
