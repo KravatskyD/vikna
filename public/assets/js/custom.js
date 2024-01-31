0dfac64c8ab6d400a6099a67c8223ddfa085e15d
ihorPetrov <garik.igor.petrov@gmail.com>
2024-01-31 11:12:34 +0200

    fix

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -7,29 +7,33 @@ function getYear() {
 getYear();
 
 
-$('.custom_slick_slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    fade: true,
-    adaptiveHeight: true,
-    asNavFor: '.slick_slider_nav',
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-            dots: false
-        }
-    }]
+document.addEventListener('load', () => {
+    $('.custom_slick_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        fade: true,
+        adaptiveHeight: true,
+        asNavFor: '.slick_slider_nav',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: false
+            }
+        }]
+    })
+
+    $('.slick_slider_nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.custom_slick_slider',
+        centerMode: false,
+        focusOnSelect: true,
+        variableWidth: true
+    });
 })
 
-$('.slick_slider_nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.custom_slick_slider',
-    centerMode: false,
-    focusOnSelect: true,
-    variableWidth: true
-});
+
 
 
 /** google_map js **/
